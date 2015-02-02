a973868e68460355eaf2c45c3c350a4b853d4ffa
Angel <angel.corral71@gmail.com>
2015-02-02 12:46:34 -0600

    project creation

---
+++ Assignment1/src/assignment1/Assignment1Controller.java
@@ -0,0 +1,268 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+//
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import javafx.collections.MapChangeListener;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.layout.AnchorPane;
+import java.util.logging.Logger;
+import java.util.logging.Handler;
+import java.util.logging.FileHandler;
+import java.util.logging.ConsoleHandler;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import java.io.IOException;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.application.Platform;
+import javafx.scene.input.ContextMenuEvent;
+
+/**
+ *
+ * @author angel
+ */
+public class Assignment1Controller implements Initializable {
+    
+    private Label label;
+    @FXML
+    private TextField firstnameTextField;
+    @FXML
+    private TextField lastnameTextField;
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField salaryTextField;
+    @FXML
+    private TextField phoneTextField;
+    @FXML
+    private TextField addressTextField;
+    @FXML
+    private TreeView<Employee> employeeTreeView;
+    @FXML
+    private Button updateButton;
+    //Create logger and employee tree manager objects
+    private static final Logger logger = Logger.getLogger(
+                    Assignment1Controller.class.getName());
+    private final EmployeeManager em = EmployeeManager.getInstance();
+    private Employee theEmployee = null;
+    private boolean changeOK = false;
+    private BooleanProperty enableUpdateProperty;
+    @FXML
+    private TextField idTextField;
+    
+    @FXML
+    private Button clearButton;
+    private BooleanProperty enableClearProperty;
+    
+    @FXML
+    private Button deleteButton;
+    private BooleanProperty enableDeleteProperty;
+    @FXML
+    private Button addButton;
+    private BooleanProperty enableAddProperty;
+    
+    
+
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        logger.setLevel(Level.FINE);
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+        logger.addHandler(handler);
+        
+        try{
+            FileHandler fileHandler = new FileHandler();
+            //records sent to file javaN.log in user's home directory
+            fileHandler.setLevel(Level.FINE);
+            logger.addHandler(fileHandler);
+            logger.log(Level.FINE, "Created File Handler");
+            
+            }catch (IOException | SecurityException ex){
+                logger.log(Level.SEVERE, "Couldn't create FileHandler", ex);
+            }
+        
+        enableUpdateProperty = new SimpleBooleanProperty(
+                this, "enableUpdate", false);
+        updateButton.disableProperty().bind(enableUpdateProperty.not());  
+        
+        enableClearProperty = new SimpleBooleanProperty(
+                this, "enableClear", true);
+        clearButton.disableProperty().bind(enableClearProperty.not());
+       
+        
+        enableDeleteProperty = new SimpleBooleanProperty(
+                this, "enableDelete", true);
+        deleteButton.disableProperty().bind(enableDeleteProperty.not());
+        
+        enableAddProperty = new SimpleBooleanProperty(
+                this, "enableAdd", false);
+        addButton.disableProperty().bind(enableAddProperty.not());
+        
+        
+        
+        buildData();
+        TreeItem<Employee> rootNode = new TreeItem<>(
+                new Employee("John","Doe"));
+        buildTreeView(rootNode);
+        employeeTreeView.setRoot(rootNode);
+        employeeTreeView.getRoot().setExpanded(true);
+        employeeTreeView.getSelectionModel().selectedItemProperty()
+                .addListener(treeSelectionListener);
+        
+    }    
+    
+    private void buildData(){
+        em.addEmployee(new Employee("Bill", "Clinton", "Employee"));
+        em.addEmployee(new Employee("Jane", "Doe", "Secretary"));
+        em.addEmployee(new Employee("Michael", "Jordan", "Supervisor"));
+        em.addEmployee(new Employee());
+        logger.log(Level.FINE, em.getAllEmployees().toString());
+    }
+    private void buildTreeView(TreeItem<Employee> root){
+        
+        em.addListener(employeeTreeListener);
+        
+        em.getAllEmployees().stream().forEach((p)->{
+            root.getChildren().add(new TreeItem<>(p));
+        });
+    }
+    
+    private final ChangeListener<TreeItem<Employee>> treeSelectionListener =
+            (ov, oldValue, newValue) -> {
+                TreeItem<Employee> treeItem = newValue;
+                logger.log(Level.FINE, "selected item = {0}", treeItem);
+                enableUpdateProperty.set(false);
+                changeOK = false;
+                if (treeItem == null || treeItem.equals(employeeTreeView.getRoot())){
+                    clearForm();
+                    return;
+                }
+                theEmployee = new Employee(treeItem.getValue());
+                logger.log(Level.FINE, "selected employee = {0}", theEmployee);
+                configureEditPanelBindings(theEmployee);
+                
+                changeOK = true;
+            };
+    
+    private void configureEditPanelBindings(Employee e){
+        firstnameTextField.textProperty()
+                .bindBidirectional(e.firstNameProperty());
+        lastnameTextField.textProperty()
+                .bindBidirectional(e.lastNameProperty());
+        titleTextField.textProperty()
+                .bindBidirectional(e.titleProperty());
+        phoneTextField.textProperty()
+                .bindBidirectional(e.phoneProperty());
+        salaryTextField.textProperty()
+                .bindBidirectional(e.salaryProperty());
+        addressTextField.textProperty()
+                .bindBidirectional(e.addressProperty());
+        idTextField.setText(Long.toString(e.getId()));
+        
+    }
+    
+    private void clearForm(){
+        firstnameTextField.setText("");
+        lastnameTextField.setText("");
+        titleTextField.setText("");
+        phoneTextField.setText("");
+        salaryTextField.setText("");
+        addressTextField.setText("");
+        idTextField.setText("");
+    }
+
+    @FXML
+    private void handleKeyAction(KeyEvent event) {
+        if(changeOK){
+            enableUpdateProperty.set(true);
+            
+        }
+    }
+    
+    private final MapChangeListener<Long, Employee> employeeTreeListener = 
+            change -> {
+                if (Platform.isFxApplicationThread()){
+                    logger.log(Level.FINE, "Is JavaFX Application Thread");
+                    updateTree(change);
+                }else{
+                    logger.log(Level.FINE, "Is BACKGROUND Thread");
+                    Platform.runLater(() -> updateTree(change));
+                }
+            };
+    
+    private void updateTree(MapChangeListener.Change<? extends Long, ? extends Employee> change){
+        if (change.getValueAdded() != null){
+            for (TreeItem<Employee> node : employeeTreeView.getRoot().getChildren() ){
+                if (change.getKey().equals(node.getValue().getId())){
+                    node.setValue(change.getValueAdded());
+                    return;
+                }
+            }
+        }
+    }
+    
+    private void updateButtonAction(ActionEvent event) {
+        enableUpdateProperty.set(false);
+        em.updateEmployee(theEmployee);
+        
+    }
+
+    private void clearButtonAction(ActionEvent event) {
+        enableClearProperty.set(false);
+       clearForm();
+    }
+
+
+    private void deleteButtonAction(ActionEvent event) {
+        enableDeleteProperty.set(true);
+        em.deleteEmployee(theEmployee);
+        clearForm();
+        
+    }
+
+    private void addButtonAction(ActionEvent event) {
+        enableAddProperty.set(true);
+        Employee e = new Employee(firstnameTextField.getText(), lastnameTextField.getText());
+        em.addEmployee(theEmployee);
+    }
+
+    @FXML
+    private void clearButtonAction(MouseEvent event) {
+        clearForm();
+    }
+
+    @FXML
+    private void deleteButtonAction(MouseEvent event) {
+       em.deleteEmployee(theEmployee);
+       clearForm();
+    }
+
+    @FXML
+    private void addButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void updateButtonAction(ContextMenuEvent event) {
+    }
+
+    
+}
---
+++ Assignment1/src/assignment1/Employee.java
@@ -0,0 +1,261 @@
+/*
+ * This class creates employee objects with some basic information
+ * and default values for the fields are given in the default constructor
+ * equals() and toString() methods are overwritten
+ * 
+ */
+package assignment1;
+
+//
+import java.util.Objects;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleStringProperty;
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * @author Jose Corral
+ */
+@SuppressWarnings("EqualsAndHashcode")
+public class Employee implements Serializable {
+    
+    private final StringProperty firstname = 
+            new SimpleStringProperty(this, "firstname", "");
+    private final StringProperty lastname =
+            new SimpleStringProperty(this, "lastname", "");
+    private final StringProperty phone =
+            new SimpleStringProperty(this, "phone", "");
+    private final StringProperty title =
+            new SimpleStringProperty(this, "title", "");
+    private final StringProperty salary =
+            new SimpleStringProperty(this, "salary");
+    private final StringProperty address =
+            new SimpleStringProperty(this, "address");
+    private final long id;
+    private static long count = 1000;
+    
+    /*
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private String id;
+    private String title;
+    private int salary;
+    private java.util.Date dateCreated; */
+
+    // Constructors for Employee class
+    // The first constructor is the default constructor and it is called from the
+    // other constructors
+
+
+    public Employee() {
+        this.firstname.set("John");
+        this.lastname.set("Doe");
+        this.phone.set("630-555-1234");
+        this.salary.set("45000");
+        this.title.set("Employee");
+        this.address.set("123 Main St.");
+        this.id = count++;
+        
+        /*
+        this.firstname = "John";
+        this.lastName = "Doe";
+        this.phoneNumber = "630-555-1234";
+        this.address = "123 Main St.";
+        this.id = "00000";
+        this.title = "Employee";
+        this.salary = 45000;
+        this.dateCreated = new Date(); */
+    }
+
+    public Employee(String first, String last) {
+        this();
+        this.firstname.set(first);
+        this.lastname.set(last);
+        this.title.set("Employed");
+        //this.firstName = firstName;
+        
+    }
+
+    public Employee(String first, String last, String title) {
+        this();
+        this.firstname.set(first);
+        this.lastname.set(last);
+        this.title.set(title);
+    }
+    public Employee(Employee emp){
+        this.firstname.set(emp.getFirstname());
+        this.lastname.set(emp.getLast());
+        this.title.set(emp.getTitle());
+        this.id = emp.getId();
+        this.phone.set(emp.getPhone());
+        this.address.set(emp.getAddress());
+        this.salary.set(emp.getSalary()); 
+        
+    }
+
+    // Setter Methods for Employee data fields
+   /*Setter method for employee first name
+    @param firstName This is the parameter of the setFirstName method
+    @return void No return value
+    */
+    public void setFirstName(String first) {
+        this.firstname.set(first);
+    }
+
+    /*Setter method for employee last name
+    @param lastName This is the parameter to the setLastName method
+    @return void No return value
+    */
+    public void setLastName(String last) {
+        this.lastname.set(last); 
+    }
+
+    /*Setter method for the employee phone number
+    @param phone This is the parameter to the setPhone method
+    @return void No return value
+    */
+    public void setPhone(String phone) {
+        this.phone.set(phone);
+    }
+
+    /*Setter method for the employee address
+    @param address This is the parameter to the setAddress method
+    @return void No return value
+    */
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+
+
+    /*Setter method for the employee title
+    @param title This is the parameter for the setTitle method
+    @return void No return value
+    */
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    /*Setter method for the employee salary
+    @param salary This is the parameter for the salary method
+    @return void No return value
+    */
+    public void setSalary(String salary) {
+        this.salary.set(salary);
+    }
+
+
+    // Getter Methods for Employee data fields
+   /* Getter method for the first name of employee
+   @param args Unused
+   @return string value of firstName
+   */
+    public String getFirstname() {
+        return firstname.get();
+    }
+
+    /* Getter method for the last name of employee
+   @param args Unused
+   @return String value of lastName
+   */
+    public String getLast() {
+        return lastname.get();
+    }
+
+    /* Getter method for the employee id
+    @param args Unused
+    @return int value of the id
+    */
+    public final long getId() {
+        return id;
+    }
+
+    /*Getter method for the employee phone number
+    @param args Unused
+    @return String value of phoneNumber
+    */
+    public String getPhone() {
+        return phone.get();
+    }
+
+    /*Getter method for the employee address
+    @param args Unused
+    @return String value of the address
+    */
+    public String getAddress() {
+        return address.get();
+    }
+
+    /*Getter method for the employee title
+    @param args Unused
+    @return String value of the title
+    */
+    public String getTitle() {
+        return title.get();
+    }
+
+    /*Getter method for the employee salary
+    @param args Unused
+    @return int value of the salary
+    */
+    public String getSalary() {
+        return salary.get();
+    }
+    
+    public StringProperty firstNameProperty(){
+        return firstname;
+    }
+    
+    public StringProperty lastNameProperty(){
+        return lastname;
+    }
+    
+    public StringProperty phoneProperty(){
+        return phone;
+    }
+    
+    public StringProperty addressProperty(){
+        return address;
+    }
+    
+    public StringProperty titleProperty(){
+        return title;
+    }
+    public StringProperty salaryProperty(){
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstname.get() + '\'' +
+                ", lastName='" + lastname.get() + '\'' +
+                ", phoneNumber='" + phone.get() + '\'' +
+                ", address='" + address.get() + '\'' +
+                ", ID='" + id + '\'' +
+                ", title='" + title.get() + '\'' +
+                ", salary='" + salary.get() + '\'' +
+                '}';
+    }
+
+    /* the equals() method is checking whether two employees have the same
+        employee id as this is the only thing that is unique to each employee
+        @param args an object which will be type casted to an Employee type
+        @return boolean true or false
+        */
+    @Override
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    public boolean equals(Object e) {
+        return id == ((Employee) e).getId();
+    }
+
+    /**
+     * @param e is an Employee object
+     */
+    // This is a class method that will print the information for an employee object
+    public static void printEmployee(Employee e) {
+        System.out.print(e.firstname + " " + e.lastname + "\t" + e.phone + "\t" + "$" + e.salary + "\t" + e.address
+                + "\t" + e.id + "\t" + e.title + "\n");
+    }
+
+}
